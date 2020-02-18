bcab83a4862f712347f8e139e8ffd0cd84d18b95
Max Edgar <max.edgar@creditsafe.com>
2020-02-18 21:06:17 +0000

    Clean up

--- ddns.ps1
+++ ddns.ps1
@@ -18,50 +18,46 @@ function Update-Slack {
 	}
 }
 
-
 $header = @{"Authorization"="Bearer $env:bearer"}
 $zoneid = $env:zoneid
-$LastIP = [PSCustomObject]@{IP="";LastUpdate="$(Get-Date)"}
+$LastIP = [PSCustomObject]@{IP="";DateStamp="$(Get-Date)";StatusCode=000}
 $name = $($($env:URL).split("."))[0]
+$dnsrecordid = $(Invoke-RestMethod -Method GET -Uri "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$env:URL" -Headers $header -ContentType "application/json").result.id
+
 while($true){
     if($(Test-Connection 8.8.8.8 -count 2 -quiet -InformationAction Ignore)){   # returns true if the host can be reached and ignores the loop if it cant connect.
-        $getUpdate = Invoke-WebRequest http://ifconfig.me
-	$CurrentIP = [PSCustomObject]@{IP="$(Invoke-RestMethod http://ifconfig.me/ip)";LastUpdate=$(Get-Date)}
-        if($($CurrentIP.IP) -ne $($LastIP.IP) -and $getUpdate.StatusCode -eq "200"){
-			$dnsrecordid = $(Invoke-RestMethod -Method GET -Uri "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$env:URL" -Headers $header -ContentType "application/json").result.id
+		$GetIP = $(Invoke-WebRequest http://ifconfig.me)
+		$CurrentIP = [PSCustomObject]@{IP=$GetIP.Content;DateStamp=$(Get-Date);StatusCode=$GetIP.StatusCode}
+		
+		if($($CurrentIP.IP) -ne $($LastIP.IP) -and $CurrentIP.StatusCode -eq "200"){
 			$CurrentCF = $(Invoke-RestMethod -ContentType "application/json" -Headers $header -Method GET -Uri "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid").result.content
-            if ($CurrentCF -ne $($CurrentIP.IP)){
-				Write-Host "IP Address has changed to $($CurrentIP.IP). DNS Records Need to be updated after $(NEW-TIMESPAN –Start $($LastIP.LastUpdate) –End $($CurrentIP.LastUpdate))"
+			
+			if ($CurrentCF -ne $($CurrentIP.IP)){
+				Write-Host "IP Address has changed to $($CurrentIP.IP). DNS Records Need to be updated after $(NEW-TIMESPAN –Start $($LastIP.DateStamp) –End $($CurrentIP.DateStamp))"
+				
 				if ($env:Slack -ne "Not_Set"){
 					Update-Slack -message "$env:URL has moved to $($CurrentIP.IP)"
 				}
+				
 				Write-Verbose "Updating DNS Records..."
-				$dnsrecordid = $(Invoke-RestMethod -Method GET -Uri "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$env:URL" -Headers $header -ContentType "application/json").result.id
 				$APIBody = @{ type="A"; name=$name; content="$($CurrentIP.IP)"}
 				$APIBodyJson = $APIBody | ConvertTo-Json -Depth 5
 				$Responce = Invoke-RestMethod -ContentType "application/json" -Headers $header -Method PUT -Uri "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" -Body $APIBodyJson
+				
 				if ($Responce.success -eq $true){
-					Write-Verbose "Confirming update..."
-					$CheckIP = $(Test-Connection $env:URL -count 1 -InformationAction Ignore)
-					$LiveIP = $CheckIP.Replies.Address.IPAddressToString
-					While ($($CurrentIP.IP) -ne $($LiveIP)){
-						sleep(10)
-						$CheckIP = $(Test-Connection $env:URL -count 1 -InformationAction Ignore)
-						$LiveIP = $CheckIP.Replies.Address.IPAddressToString
-					}
 					$LastIP = $CurrentIP
 					Write-Host "Live IP Updated to $LiveIP"
 				} else {
 					break
 				}
-			} else {
-			$LastIP = @{IP="$CurrentCF";LastUpdate="$(Get-Date)"}
+			} elseif ($CurrentCF -eq $($CurrentIP.IP)) {
+			$LastIP = @{IP="$CurrentCF";DateStamp="$(Get-Date)"}
 			Write-Host "$env:URL`nCurrent IP address ($CurrentCF) is already Live on CloudFLare.`nNo action requred"
 			Update-Slack -message "$env:URL`nCurrent IP address ($CurrentCF) is already Live on CloudFLare.`n*No action requred*"
 			}
 			
         }
-        sleep($env:poll)
+        Start-Sleep($env:poll)
     }
     
 }
